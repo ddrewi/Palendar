036e19d755217d3efc71931257c756c90d47fe2c
markariagno22 <Mark.ariagno14@gmail.com>
2022-10-31 12:52:45 -0500

    implemented onClickListener

--- app/src/main/java/com/example/palendar/FillScheduleActivity.java
+++ app/src/main/java/com/example/palendar/FillScheduleActivity.java
@@ -3,6 +3,10 @@ package com.example.palendar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewParent;
+import android.widget.ToggleButton;
 
 public class FillScheduleActivity extends AppCompatActivity {
 
@@ -11,4 +15,13 @@ public class FillScheduleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fill_schedule);
     }
+
+
+    ToggleButton toggleButton = (ToggleButton) findViewById(R.id.testToggleButton);
+    View.OnHoverListener
+    toggleButton.(new View.OnClickListener() {
+        public void onClick(View v) {
+            startActivity(new Intent(MainActivity.this,StandingsActivity.class));
+        }
+    });
 }

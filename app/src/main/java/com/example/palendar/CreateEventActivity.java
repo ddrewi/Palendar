09e200fee5f46547f701b856d14326645fd2f275
markariagno22 <Mark.ariagno14@gmail.com>
2022-11-09 12:54:49 -0600

    continued work on editable calendar

--- app/src/main/java/com/example/palendar/CreateEventActivity.java
+++ app/src/main/java/com/example/palendar/CreateEventActivity.java
@@ -2,13 +2,39 @@ package com.example.palendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+
+import java.util.ArrayList;
 
 public class CreateEventActivity extends AppCompatActivity {
+
+    public static final String ARRAYLIST_VALUES = "Data to display in gridView";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> listView,
+                                    View itemView, int position, long id) {
+                Intent intent = new Intent(CreateEventActivity.this, FillScheduleActivity.class);
+
+
+                // create an ArrayList of whatever type of data you are displaying in this app
+                // In this particular app, everything is centralized around the Food class.
+                ArrayList<String> listData = new ArrayList<>();
+
+
+                // Send this particular ArrayList of Food data to the next activity, where we display
+                // each name of each object in the ArrayList.
+                intent.putStringArrayListExtra(ARRAYLIST_VALUES, listData);
+                startActivity(intent);
+
+            }
+        };
     }
 }
--- app/src/main/java/com/example/palendar/FillScheduleActivity.java
+++ app/src/main/java/com/example/palendar/FillScheduleActivity.java
@@ -3,13 +3,19 @@ package com.example.palendar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewParent;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.GridView;
 import android.widget.ToggleButton;
+
+import java.util.ArrayList;
 
 public class FillScheduleActivity extends AppCompatActivity {
 
@@ -19,6 +25,16 @@ public class FillScheduleActivity extends AppCompatActivity {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_fill_schedule);
 
+            Intent intent = getIntent();
+
+            ArrayList<String> dataToDisplay = intent.getStringArrayListExtra(CreateEventActivity.ARRAYLIST_VALUES);
+
+            ArrayAdapter<String> listAdapter = new ArrayAdapter<>(
+                    this, android.R.layout.simple_list_item_1, dataToDisplay);
+
+            GridView gridView = (GridView) findViewById(R.id.timeOptions);
+            Log.i("Mark", "grid");
+            gridView.setAdapter(listAdapter);
 
         }
 

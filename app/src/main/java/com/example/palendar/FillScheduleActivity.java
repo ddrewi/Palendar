ddfb126b0a4a7d5df7f556b05cfc01b9939dc55a
markariagno22 <Mark.ariagno14@gmail.com>
2022-11-10 12:53:42 -0600

    continued work on editable gridView

--- app/src/main/java/com/example/palendar/FillScheduleActivity.java
+++ app/src/main/java/com/example/palendar/FillScheduleActivity.java
@@ -9,11 +9,14 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewParent;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.GridView;
 import android.widget.ToggleButton;
+
+import org.checkerframework.checker.units.qual.A;
 
 import java.util.ArrayList;
 
@@ -27,14 +30,42 @@ public class FillScheduleActivity extends AppCompatActivity {
 
             Intent intent = getIntent();
 
-            ArrayList<String> dataToDisplay = intent.getStringArrayListExtra(CreateEventActivity.ARRAYLIST_VALUES);
+            // ArrayList<String> dataToDisplay = intent.getStringArrayListExtra(CreateEventActivity.ARRAYLIST_VALUES);
+
+            ArrayList<String> dataToDisplay = new ArrayList<String>();
+
+            dataToDisplay.add("string1");
+            dataToDisplay.add("string2");
+            dataToDisplay.add("string1");
+            dataToDisplay.add("string2");
+            dataToDisplay.add("string1");
+            dataToDisplay.add("string2");
+            dataToDisplay.add("string1");
+            dataToDisplay.add("string2");
+            dataToDisplay.add("string1");
+            dataToDisplay.add("string2");
+            dataToDisplay.add("string1");
+            dataToDisplay.add("string2");
+            dataToDisplay.add("string1");
+            dataToDisplay.add("string2");
+            dataToDisplay.add("string1");
+            dataToDisplay.add("string2");
 
             ArrayAdapter<String> listAdapter = new ArrayAdapter<>(
                     this, android.R.layout.simple_list_item_1, dataToDisplay);
 
             GridView gridView = (GridView) findViewById(R.id.timeOptions);
-            Log.i("Mark", "grid");
-            gridView.setAdapter(listAdapter);
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, dataToDisplay);
+
+            gridView.setAdapter(adapter);
+
+//            for(int i = 0; i < dataToDisplay.size(); i++){
+//                ToggleButton toggleButton = findViewById(R.id.toggleButton00
+//                toggleButton.setTextOff(dataToDisplay.get(i));
+//                toggleButton.setTextOff(dataToDisplay.get(i));
+//            }
+
 
         }
 
